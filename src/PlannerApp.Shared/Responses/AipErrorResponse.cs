c10fc7375a6b27d609a2074258247592e3133c3f
Ninja <persona.nc@outlook.com>
2021-08-09 22:02:43 +0630

    Create the project & initialize MudBulazor

---
+++ src/PlannerApp.Shared/Responses/AipErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace PlannerApp.Shared.Responses
+{
+    public class AipErrorResponse
+    {
+        public string Message { get; set; }
+        public string[] Errors { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}
---
+++ src/PlannerApp/AuthorizationMessageHandler.cs
@@ -0,0 +1,31 @@
+﻿using Blazored.LocalStorage;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PlannerApp
+{
+    public partial class Program
+    {
+        public class AuthorizationMessageHandler : DelegatingHandler
+        {
+            private readonly ILocalStorageService storage;
+
+            public AuthorizationMessageHandler(ILocalStorageService storage)
+            {
+                this.storage = storage;
+            }
+            protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,CancellationToken cancellationToken)
+            {
+                if( await storage.ContainKeyAsync("access_token"))
+                {
+                    var token = await storage.GetItemAsStringAsync("access_token");
+                    request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                }
+
+                return await base.SendAsync(request, cancellationToken);
+            }
+        }
+    }
+}
+
